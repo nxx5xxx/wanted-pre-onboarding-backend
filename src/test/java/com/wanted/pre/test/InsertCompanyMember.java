a44bb851721cdb9cfcfc58ac1c02fe279c7b7889
nxx5xxx <dnwnx123@naver.com>
2023-10-16 17:54:21 +0900

    modify note,junit test for Entity
    Entity교체에 따른 junit 수정 및 노트추가

---
+++ src/test/java/com/wanted/pre/test/InsertCompanyMember.java
@@ -0,0 +1,40 @@
+package com.wanted.pre.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.wanted.pre.entity.Company;
+import com.wanted.pre.entity.Member;
+import com.wanted.pre.repository.CompanyRepository;
+import com.wanted.pre.repository.MemberRepository;
+
+@SpringBootTest
+class InsertCompanyMember {
+	@Autowired
+	CompanyRepository comRepo;
+	@Autowired
+	MemberRepository memRepo;
+	
+	@Test
+	@DisplayName("회사등록 테스트")
+	void insertCompany() {
+		Company comp = new Company();
+		comp.setComId("네이버");
+		comp.setCountry("대한민국");
+		comp.setRegion("분당");
+		comRepo.save(comp);
+	}
+	
+	@Test
+	@DisplayName("유저 등록 테스트")
+	void insertMember() {
+		Member mem = new Member();
+		mem.setMemberId("test01");
+		//System.out.println(mem.getMemberId());
+		//mem.setRecrList(null);
+		memRepo.save(mem);
+	}
+
+}
