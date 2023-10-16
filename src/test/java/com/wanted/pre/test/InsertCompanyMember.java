c554ac7095936a76d008a6029409b21012fc5e43
nxx5xxx <dnwnx123@naver.com>
2023-10-16 20:35:47 +0900

    modify junit test
    회사등록 하나늘림

--- src/test/java/com/wanted/pre/test/InsertCompanyMember.java
+++ src/test/java/com/wanted/pre/test/InsertCompanyMember.java
@@ -28,6 +28,16 @@ class InsertCompanyMember {
 	}
 	
 	@Test
+	@DisplayName("회사등록 테스트")
+	void insertCompany2() {
+		Company comp = new Company();
+		comp.setComId("원티드");
+		comp.setCountry("대한민국");
+		comp.setRegion("성남");
+		comRepo.save(comp);
+	}
+	
+	@Test
 	@DisplayName("유저 등록 테스트")
 	void insertMember() {
 		Member mem = new Member();
